3bccd275a0e9202d8c92957e799a32fc1c85d12c
Stephen Haberman <stephen.haberman@gmail.com>
2018-03-20 13:41:39 -0500

    Wrap in namespaces.

--- src/state2.ts
+++ src/state2.ts
@@ -1,93 +1,103 @@
 
-type AccountState = "OPEN" | "HELD" | "CLOSED";
-type AccountEvent = "deposit" | "withdraw" | "available" | "placeHold" | "removeHold" | "close" | "reopen";
+namespace state2 {
+  // define our states
+  type AccountState = "OPEN" | "HELD" | "CLOSED";
+  // and events (some events lead to transitions, e.g. placeHold,
+  // others do not, e.g. deposit)
+  type AccountEvent = "deposit" | "withdraw" | "available" | "placeHold" | "removeHold" | "close" | "reopen";
 
-type AccountBehavior = {
-  [key in AccountState]: StateBehavior
-}
-type StateBehavior = {
-  [key in AccountEvent]: EventBehavior | null;
-}
-type EventBehavior = {
-  nextState?: AccountState;
-  predicate?: (Account) => boolean
-}
-
-const behavior: AccountBehavior = {
-  OPEN: {
-    deposit: {},
-    withdraw: {},
-    available: {},
-    placeHold: { nextState: "HELD" },
-    removeHold: null,
-    close: { nextState: "CLOSED" },
-    reopen: null
-  },
-  HELD: {
-    deposit: {},
-    withdraw: null,
-    // not right, should return 0 instead of actual balance
-    available: {},
-    placeHold: null,
-    removeHold: { nextState: "OPEN" },
-    close: { nextState: "CLOSED" },
-    reopen: null,
-  },
-  CLOSED: {
-    deposit: null,
-    withdraw: null,
-    available: null,
-    placeHold: null,
-    removeHold: null,
-    close: null,
-    reopen: { nextState: "OPEN" }
+  // define the shape of our config DSL
+  type AccountBehavior = {
+    // every state must have it's behavior specified
+    [key in AccountState]: StateBehavior
   }
-};
-
-class Account {
-  balance: number = 0;
-  state: AccountState = "OPEN";
-
-  deposit(amount: number) {
-    this.perform("deposit", () => this.balance += amount);
+  type StateBehavior = {
+    // for a given state, we may have an event defined; if
+    // there is no event, that means it's not a valid operation
+    // or transition in the current state, and will be rejected
+    [key in AccountEvent]?: EventBehavior;
+  }
+  type EventBehavior = {
+    // optionally defines the next state to transition to
+    nextState?: AccountState;
   }
 
-  withdraw(amount: number) {
-    this.perform("withdraw", () => this.balance -= amount);
-  }
-
-  available(): number {
-    return this.perform("available", () => this.balance);
-  }
-
-  placeHold() {
-    this.perform("placeHold", () => null);
-  }
-
-  removeHold() {
-    this.perform("removeHold", () => null);
-  }
-
-  mayDo(event: AccountEvent): boolean {
-    return behavior[this.state][event] != null;
-  }
-
-  private perform<T>(event: AccountEvent, logic: () => T): T {
-    if (!this.mayDo(event)) {
-      // not allowed, throw error
+  // now define our actual behavior, to match the types above
+  const behavior: AccountBehavior = {
+    OPEN: {
+      // since deposit is defined, it is a valid event within
+      // the OPEN state. the empty hash means we don't have
+      // a nextState to transition to next, so will stay in OPEN
+      deposit: {},
+      withdraw: {},
+      available: {},
+      placeHold: { nextState: "HELD" },
+      close: { nextState: "CLOSED" },
+    },
+    HELD: {
+      deposit: {},
+      // in theory we'd like to disable/override the behavior of
+      // available within the HELD state; our current DSL can't
+      // support that
+      available: {},
+      removeHold: { nextState: "OPEN" },
+      close: { nextState: "CLOSED" },
+    },
+    CLOSED: {
+      reopen: { nextState: "OPEN" }
     }
-    let value = logic();
-    this.transitionIfNeeded(event);
-    return value;
-  }
+  };
 
-  private transitionIfNeeded(event: AccountEvent): void {
-    let eb = behavior[this.state][event];
-    if (eb == null || eb.nextState == null) {
-      // error, not allowed
-    } else {
-      this.state = eb.nextState;
+  // now a pretty basic class with state/operations
+  class Account {
+
+    balance: number = 0;
+    state: AccountState = "OPEN";
+
+    deposit(amount: number) {
+      // each of our events goes through the "perform" method, which
+      // ensures: a) that event may be validly executed in the current
+      // state, and b) transitions us to the next state if needed
+      this.perform("deposit", () => this.balance += amount);
+    }
+
+    withdraw(amount: number) {
+      this.perform("withdraw", () => this.balance -= amount);
+    }
+
+    available(): number {
+      return this.perform("available", () => this.balance);
+    }
+
+    placeHold() {
+      this.perform("placeHold", () => null);
+    }
+
+    removeHold() {
+      this.perform("removeHold", () => null);
+    }
+
+    // allows clients to ask if a given event is allowed in our current state
+    mayDo(event: AccountEvent): boolean {
+      return behavior[this.state][event] != null;
+    }
+
+    private perform<T>(event: AccountEvent, logic: () => T): T {
+      if (!this.mayDo(event)) {
+        // not allowed, throw error
+      }
+      let value = logic();
+      this.transitionIfNeeded(event);
+      return value;
+    }
+
+    private transitionIfNeeded(event: AccountEvent): void {
+      let eb = behavior[this.state][event];
+      if (eb == null || eb.nextState == null) {
+        // error, not allowed
+      } else {
+        this.state = eb.nextState;
+      }
     }
   }
-
 }
--- src/state3.ts
+++ src/state3.ts
@@ -1,117 +1,147 @@
 
-class DefaultEventLogic {
-  deposit(account: Account, amount: number) {
-    account.balance += amount;
+namespace state3 {
+  // To allow our DSL to override behavior without being repetitive
+  // (e.g. re-defining the `deposit` implementation in multiple states),
+  // we pull our default implementations out into a stateless "business
+  // logic" class.
+  //
+  // This is somewhat odd, removing behavior from the object, but we're
+  // doing this explicitly because the logic was (in a less trivial app)
+  // becoming too complicated to live within a single object/method.
+  //
+  // This is stateless so that we can define override behavior once within
+  // our config DSL (see below) and have it apply to multiple account instances.
+  class DefaultEventLogic {
+    deposit(account: Account, amount: number) {
+      account.balance += amount;
+    }
+
+    withdraw(account: Account, amount: number) {
+      account.balance -= amount;
+    }
+
+    available(account: Account): number {
+      return account.balance;
+    }
+
+    // these events don't have any logic, as they are purely transition,
+    // but we define them here anyway so that they automatically become
+    // part of our AccountEvent type
+    placeHold() {
+    }
+
+    removeHold() {
+    }
+
+    close() {
+    }
+
+    reopen() {
+    }
   }
 
-  withdraw(account: Account, amount: number) {
-    account.balance -= amount;
+  // same basic states as before
+  type AccountState = "OPEN" | "HELD" | "CLOSED";
+  // instead of re-typing the constants `deposit | withdraw | ...`,
+  // we can copy them out of our logic class
+  type AccountEvent = keyof DefaultEventLogic;
+
+  // same trio of Account/State/Event spec behavior
+  type AccountBehavior = {
+    [key in AccountState]: StateBehavior
+  }
+  type StateBehavior = {
+    [key in AccountEvent]?: EventBehavior;
+  }
+  type EventBehavior = {
+    nextState?: AccountState,
+    override?: DefaultEventLogic;
   }
 
-  available(account: Account): number {
-    return account.balance;
-  }
-
-  placeHold() {
-  }
-
-  removeHold() {
-  }
-
-  close() {
-  }
-
-  reopen() {
-  }
-}
-
-type AccountState = "OPEN" | "HELD" | "CLOSED";
-type AccountEvent = keyof DefaultEventLogic;
-
-type AccountBehavior = {
-  [key in AccountState]: StateBehavior
-}
-type StateBehavior = {
-  [key in AccountEvent]?: EventBehavior;
-}
-type EventBehavior = {
-  nextState?: AccountState,
-  override?: DefaultEventLogic;
-}
-
-const behavior: AccountBehavior = {
-  OPEN: {
-    deposit: {},
-    withdraw: {},
-    available: {},
-    placeHold: { nextState: "HELD" },
-    close: { nextState: "CLOSED" },
-  },
-  HELD: {
-    deposit: {},
-    available: {
-      override: new class extends DefaultEventLogic {
-        available(account: Account) { return 0; }
-      }
+  // and now the actual config DSL
+  const behavior: AccountBehavior = {
+    OPEN: {
+      // same as previous, deposit is a valid operation
+      // in the OPEN state, but is not a transition
+      deposit: {},
+      withdraw: {},
+      available: {},
+      placeHold: { nextState: "HELD" },
+      close: { nextState: "CLOSED" },
     },
-    removeHold: { nextState: "OPEN" },
-    close: { nextState: "CLOSED" },
-  },
-  CLOSED: {
-    reopen: { nextState: "OPEN" }
-  }
-};
-
-class Account {
-  balance: number = 0;
-  state: AccountState = "OPEN";
-
-  deposit(amount: number) {
-    this.perform("deposit", l => l.deposit(this, amount));
-  }
-
-  withdraw(amount: number) {
-    this.perform("withdraw", l => l.withdraw(this, amount));
-  }
-
-  available(): number {
-    return this.perform("available", l => l.available(this));
-  }
-
-  placeHold() {
-    this.perform("placeHold", l => l.placeHold());
-  }
-
-  removeHold() {
-    this.perform("removeHold", l => l.removeHold());
-  }
-
-  mayDo(event: AccountEvent): boolean {
-    return behavior[this.state][event] != null;
-  }
-
-  private perform<T>(event: AccountEvent, callback: (e: DefaultEventLogic) => T): T {
-    if (!this.mayDo(event)) {
-      // not allowed, throw error
+    HELD: {
+      deposit: {},
+      available: {
+        // now we can override the `available` method specific to
+        // our HELD state. oddly, there is no override keyword in TS,
+        // so the signature of `available` is not technically type
+        // checked against the base class. See TypeScript issue #2000.
+        override: new class extends DefaultEventLogic {
+          available(account: Account) { return 0; }
+        }
+      },
+      removeHold: { nextState: "OPEN" },
+      close: { nextState: "CLOSED" },
+    },
+    CLOSED: {
+      reopen: { nextState: "OPEN" }
     }
-    let logic = this.getLogic(event);
-    let value = callback(logic);
-    this.transitionIfNeeded(event);
-    return value;
-  }
+  };
 
-  private transitionIfNeeded(event: AccountEvent): void {
-    let eb = behavior[this.state][event];
-    if (eb == null || eb.nextState == null) {
-      // error, not allowed
-    } else {
-      this.state = eb.nextState;
+  class Account {
+    balance: number = 0;
+    state: AccountState = "OPEN";
+
+    deposit(amount: number) {
+      // we have a perform method, just as before, but now instead always
+      // call the business logic within the EventLogic class, which will
+      // either be the default behavior, or the state-specific overridden
+      // behavior
+      this.perform("deposit", l => l.deposit(this, amount));
+    }
+
+    withdraw(amount: number) {
+      this.perform("withdraw", l => l.withdraw(this, amount));
+    }
+
+    available(): number {
+      return this.perform("available", l => l.available(this));
+    }
+
+    placeHold() {
+      this.perform("placeHold", l => l.placeHold());
+    }
+
+    removeHold() {
+      this.perform("removeHold", l => l.removeHold());
+    }
+
+    mayDo(event: AccountEvent): boolean {
+      return behavior[this.state][event] != null;
+    }
+
+    private perform<T>(event: AccountEvent, callback: (e: DefaultEventLogic) => T): T {
+      if (!this.mayDo(event)) {
+        // not allowed, throw error
+      }
+      let logic = this.getLogic(event);
+      let value = callback(logic);
+      this.transitionIfNeeded(event);
+      return value;
+    }
+
+    private transitionIfNeeded(event: AccountEvent): void {
+      let eb = behavior[this.state][event];
+      if (eb == null || eb.nextState == null) {
+        // error, not allowed
+      } else {
+        this.state = eb.nextState;
+      }
+    }
+
+    private getLogic(event: AccountEvent): DefaultEventLogic {
+      let eb = behavior[this.state][event];
+      return (eb && eb.override) ? eb.override : new DefaultEventLogic();
     }
   }
-
-  private getLogic(event: AccountEvent): DefaultEventLogic {
-    let eb = behavior[this.state][event];
-    return (eb && eb.override) ? eb.override : new DefaultEventLogic();
-  }
-
 }
